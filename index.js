c08f6c1b4be9bbf3870e508120c0de2b68b5b16d
Rishabh Ravindran <rishabhravindran.matrix@gmail.com>
2024-12-29 23:31:14 -0500

    forgot initial ejs file

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = 3000
 
 app.set("view engine", "ejs");
 
+app.get('/',(req,res) =>{
+    res.render('index.ejs')
+})
+
 app.listen(port,() =>{
     console.log(chalk.bgBlue.white(`[${port}]`) + ' ' + chalk.green(`http://localhost:${port}`))
 })
